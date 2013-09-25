5fd33266a83c407a1dab1c38e2b8e144c521e658
Philip Bachman <phil.bachman@gmail.com>
2013-09-25 18:27:53 +0200

    Quick change to traning demo
    Updated test_smoothnet.m so that it runs training on synthetic data to
    demo the SimpleNet class.

--- test_smoothnet.m
+++ test_smoothnet.m
@@ -1,11 +1,13 @@
 clear;
 
 % Load data to run through network
-load('mnist_scaled.mat');
+X = randn(20000,100);
+b = randn(100,1);
+Y = (X*b >= 0) + 1;
+Y = class_inds(Y);
 
-tr_idx = randsample(1:size(X,1),30000,false);
+tr_idx = randsample(1:size(X,1),10000);
 te_idx = setdiff(1:size(X,1),tr_idx);
-te_idx = randsample(te_idx,10000,false);
 Xtr = single(X(tr_idx,:));
 Xte = single(X(te_idx,:));
 Ytr = single(Y(tr_idx,:));
@@ -14,8 +16,8 @@ Ytr_c = class_cats(Ytr);
 Yte_c = class_cats(Yte);
 clear X Y;
 
-% Create the ShepNet instance
-layer_dims = [size(Xtr,2) 500 500 250 size(Ytr,2)];
+% Create the SmoothNet instance
+layer_dims = [size(Xtr,2) 500 500 size(Ytr,2)];
 NET = SmoothNet(Xtr, Ytr, layer_dims, ActFunc(5), ActFunc(1));
 NET.init_weights(0.1);
 
@@ -34,14 +36,17 @@ end
 
 % Setup param struct for training the net
 params = struct();
-params.rounds = 30000;
+params.rounds = 10000;
 params.start_rate = 0.1;
 params.decay_rate = 0.1^(1 / params.rounds);
 params.momentum = 0.9;
-params.batch_size = 200;
+params.batch_size = 100;
 % Regularization parameters
 params.lam_l2 = 1e-4;
 % Validation stuff
 params.do_validate = 1;
 params.Xv = Xte;
 params.Yv = Yte;
+
+% Train the network on training data using the given parameters
+NET.train(Xtr,Ytr,params);
