fa431a11985bb365ceda0b4c2292954dc21600e9
Philip Bachman <phil.bachman@gmail.com>
2014-05-28 21:40:28 -0400

    a little more tidying-up
    Comments added, unused code removed.

--- LayerNet/test_ldnet.m
+++ LayerNet/test_ldnet.m
@@ -1,6 +1,6 @@
 clear;
 
-% Load USPS digits data
+% Load MNIST digits data
 load('mnist_data.mat');
 X = double(X_mnist) ./ max(max(double(X_mnist)));
 Y = LDNet.class_inds(double(Y_mnist));
@@ -8,9 +8,9 @@ Y = LDNet.class_inds(double(Y_mnist));
 
 % Setup SGD-related training options
 opts = struct();
-opts.do_draw = 1;
+opts.do_draw = 1; % draw first layer filters, set to 0 for not MNIST/USPS data
 opts.lam_l2 = 0; % standard L2 weight decay, often worse than L2 constraint
-opts.rounds = 10000;
+opts.rounds = 20000;
 opts.batch_size = 100;
 opts.dev_reps = 2; % experimental, only matters if LDN.do_dev ~= 1
 opts.do_validate = 1; % whether to check loss on validation sample
@@ -20,9 +20,10 @@ opts.Yv = Yte; % validation outputs
 %%%%%%%%%%%%
 % Init a network, given a list of layer sizes, a hidden layer activation and
 % a loss function to optimize (set loss to loss_lsq for regression.
-LDN = LDNet([size(Xtr,2) 800 800 size(Ytr,2)], @LDLayer.relu_trans, @LDNet.loss_mcl2h);
+LDN = LDNet([size(Xtr,2) 200 200 size(Ytr,2)], ...
+    @LDLayer.relu_trans, @LDNet.loss_lsq);
 LDN.lam_l2a = [0.0 0.0 0.0]; % L2 regularization on per-layer activations
-LDN.wt_bnd = 2.0; % L2 constraint on weights into each hidden node
+LDN.wt_bnd = 3.0; % L2 constraint on weights into each hidden node
 LDN.drop_input = 0.2; % drop rate at input layer
 LDN.drop_hidden = 0.5; % drop rate at hidden layers
 LDN.drop_undrop = 0.0; % rate to pass samples drop-free
@@ -31,34 +32,33 @@ LDN.drop_undrop = 0.0; % rate to pass samples drop-free
 LDN.dev_lams = [0.0 0.0 0.0];
 LDN.dev_types = [1 1 2];
 LDN.do_dev = 0;
-LDN.dev_pre = 0;
 %%%%%%%%%%%%
 
 % Initialize network weights and biases
-LDN.init_weights(0.1,0.01);
+LDN.init_weights(0.05,0.1);
 
 % Set core learning params and train a bit
 opts.momentum = 0.5;
 opts.decay_rate = 0.5^(1 / opts.rounds);
-opts.start_rate = 0.5;
+opts.start_rate = 0.1;
 LDN.train(Xtr,Ytr,opts);
 
 % Set core learning params and train a bit
 opts.momentum = 0.75;
 opts.decay_rate = 0.5^(1 / opts.rounds);
-opts.start_rate = 0.25;
+opts.start_rate = 0.05;
 LDN.train(Xtr,Ytr,opts);
 
 % Set core learning params and train a bit
 opts.momentum = 0.9;
 opts.decay_rate = 0.5^(1 / opts.rounds);
-opts.start_rate = 0.125;
+opts.start_rate = 0.02;
 LDN.train(Xtr,Ytr,opts);
 
 % Set core learning params and train a bit
 opts.momentum = 0.95;
 opts.decay_rate = 0.1^(1 / opts.rounds);
-opts.start_rate = 0.05;
+opts.start_rate = 0.01;
 LDN.train(Xtr,Ytr,opts);
 
 
